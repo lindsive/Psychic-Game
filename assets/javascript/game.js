faa4c5dac496975db548c00e076c450df118a2eb
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-06-10 15:53:01 -0400

    added body to index, began javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+
+var alphabetChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+
+    var computerGuess = alphabetChoices[Math.floor(math.random() * alphabetChoices.length)];
+
+    if ((userGuess !== computerGuess)) {
+
+    }
+
+}
