46391c374436746a724a8b33e42a6cee13b91b70
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-06-12 15:24:06 -0400

    assigned id's to html elements; programmed javascript for wins, loses, and guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,30 @@
 
 var alphabetChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var loss = 0;
+var wins= 0;
+
+var gamesWon = document.getElementById("gamesWon");
+var gamesLost = documeent.getElementById("gamesLost");
+var gameTitle = document.getElementById("heading");
+var gameDirections = document.getElementById("directions");
+var guessesLeft = document.getElementById("guessesLeft");
+
+
 document.onkeyup = function(event) {
     var userGuess = event.key;
 
     var computerGuess = alphabetChoices[Math.floor(math.random() * alphabetChoices.length)];
 
-    if ((userGuess !== computerGuess)) {
-
+    if ((userGuess === computerGuess)) {
+        wins++;
+    } else {
+        loss++;
     }
 
-}
+
+gamesWon.textContent = wins;
+gamesLost.textContent= loss;
+
+};
+
