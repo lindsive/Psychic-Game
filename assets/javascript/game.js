34e5b80c0c418bf736516a6ef9c938d63e101e5e
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-06-13 11:11:49 -0400

    removed style css; removed commented out code; added title

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,58 @@
-
-
-
 var alphabetChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var loss = 0;
-var wins= 0;
+var wins = 0;
+var guessesRemaining = 9;
+var userGuesses = [];
 
 var gamesWon = document.getElementById("gamesWon");
-var gamesLost = documeent.getElementById("gamesLost");
+var gamesLost = document.getElementById("gamesLost");
 var gameTitle = document.getElementById("heading");
 var gameDirections = document.getElementById("directions");
 var guessesLeft = document.getElementById("guessesLeft");
+var guessList = document.getElementById("guessList");
 
 
-document.onkeyup = function(event) {
-    var userGuess = event.key;
+function game1() {
 
-    var computerGuess = alphabetChoices[Math.floor(math.random() * alphabetChoices.length)];
+    document.onkeyup = function (event) {
+        var userGuess = event.key;
+        if (alphabetChoices.includes(userGuess) == true) {
 
-    if ((userGuess === computerGuess)) {
-        wins++;
-    } else {
-        loss++;
-    }
+            userGuesses.push(userGuess);
+            var computerGuess = alphabetChoices[Math.floor(Math.random() * alphabetChoices.length)];
+
+            if ((userGuess === computerGuess)) {
+                wins++;
+                restart();
+            } else {
+                guessesRemaining--;
+            }
+
+            if ((guessesRemaining === 0)) {
+                loss++;
+                restart();
+
+            }
 
 
-gamesWon.textContent = wins;
-gamesLost.textContent= loss;
+            gamesWon.textContent = "Wins: " + wins;
+            gamesLost.textContent = "Losses: " + loss;
+            guessList.textContent = "your guesses so far: " + userGuesses.join(", ");
+            guessesLeft.textContent = "Guesses Left: " + guessesRemaining;
+        }
+        else {
+            alert("Please pick a letter");
+        };
+    };
 
+};
+
+game1();
+
+function restart() {
+    guessesRemaining = 9;
+    userGuesses = [];
+    game1();
 };
 
